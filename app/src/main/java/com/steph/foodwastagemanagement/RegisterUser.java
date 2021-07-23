76fa0dc54fa3a4f711329312eb5f05442ac61d55
bridgethom-99 <thomybridge@gmail.com>
2021-07-23 22:01:16 +0300

    Authentication modifications.

--- app/src/main/java/com/steph/foodwastagemanagement/RegisterUser.java
+++ app/src/main/java/com/steph/foodwastagemanagement/RegisterUser.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.util.AndroidException;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
--- app/src/main/java/com/steph/foodwastagemanagement/User.java
+++ app/src/main/java/com/steph/foodwastagemanagement/User.java
@@ -2,10 +2,10 @@ package com.steph.foodwastagemanagement;
 
 public class User {
     public String fullName, email;
-    public User(){
+    public User(String fullName, String email){
 
     }
-    public User(String fullName,String email){
+    public User(String fullName,String email, String age){
         this.fullName=fullName;
         this.email=email;
     }
