a1fc10048b93aabff555c6d55a81f7dddf94839e
Stephanie Otieno <stephanieachieng45@gmail.com>
2021-07-29 23:08:24 +0300

    profile activity

--- app/src/main/java/com/steph/foodwastagemanagement/ProfileActivity.java
+++ app/src/main/java/com/steph/foodwastagemanagement/ProfileActivity.java
@@ -1,19 +1,16 @@
 package com.steph.foodwastagemanagement;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,7 +20,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class ProfileActivity extends AppCompatActivity implements OnItemSelectedListener{
+public class  ProfileActivity extends AppCompatActivity {
 
     private FirebaseUser user;
     private DatabaseReference reference;
@@ -32,7 +29,6 @@ public class ProfileActivity extends AppCompatActivity implements OnItemSelected
 
 
     private Button logout;
-    private String mSpinnerLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,23 +36,8 @@ public class ProfileActivity extends AppCompatActivity implements OnItemSelected
         setContentView(R.layout.activity_profile);
 
         logout = (Button) findViewById(R.id.signOut);
-       // Spinner userSpinner = (Spinner) findViewById(R.id.user_spinner);
 
-        /*if (userSpinner !=null){
-            userSpinner.setOnItemSelectedListener(this);
-        }
-
-*/
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.register_spinner, android.R.layout.simple_spinner_item);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        /*if (userSpinner !=null){
-          userSpinner.setAdapter(adapter);
-        }
-        */
-
-        logout.setOnClickListener(new OnClickListener() {
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
@@ -68,12 +49,14 @@ public class ProfileActivity extends AppCompatActivity implements OnItemSelected
         reference = FirebaseDatabase.getInstance().getReference("Users");
         userID = user.getUid();
 
+
         final TextView greetingTextView = (TextView) findViewById(R.id.greeting);
         final TextView fullNameTextView = (TextView) findViewById(R.id.fullName);
         final TextView emailTextView = (TextView) findViewById(R.id.emailAddress);
-        Spinner userSpinner = (Spinner) findViewById(R.id.user_spinner);
+        final TextView phonenumberTextView=(TextView) findViewById(R.id.phone_number);
 
-        //final TextView userTypeTextView = (TextView) findViewById(R.id.userType);
+
+
 
         reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -81,18 +64,24 @@ public class ProfileActivity extends AppCompatActivity implements OnItemSelected
                User userProfile = snapshot.getValue(User.class);
 
                if (userProfile !=null){
+
                    String fullName = userProfile.fullName;
-                   String email = userProfile.email;
-                   //Where sth could be added
+                   String email = userProfile.Email;
+                   String phonenumber=userProfile.PhoneNumber;
+
 
                    greetingTextView.setText(String.format("Welcome, %s!", fullName));
                    fullNameTextView.setText(fullName);
                    emailTextView.setText(email);
-                   userSpinner.setAdapter(adapter);
-                   //userTypeTextView.setText(user_type);
+                   phonenumberTextView.setText(phonenumber);
+
+
+
+
                }
 
             }
+
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
@@ -101,21 +90,6 @@ public class ProfileActivity extends AppCompatActivity implements OnItemSelected
             }
         });
 
-
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        mSpinnerLabel = adapterView.getItemAtPosition(i).toString();
-
-        Toast myToast = Toast.makeText(this,"Selected user is a: " +mSpinnerLabel, Toast.LENGTH_SHORT);
-        myToast.show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        Toast toast = Toast.makeText(this, "Nothing Selected", Toast.LENGTH_SHORT);
-        toast.show();
 
     }
 }
