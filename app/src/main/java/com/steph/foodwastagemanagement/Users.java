4b44701f4e097311461dbcee39c97642024f7c73
Stephanie Otieno <stephanieachieng45@gmail.com>
2021-08-22 20:48:52 +0300

    redirection of users

---
+++ app/src/main/java/com/steph/foodwastagemanagement/Users.java
@@ -0,0 +1,18 @@
+package com.steph.foodwastagemanagement;
+
+public class Users {
+    public String fullName, Email,Position,PhoneNumber;
+
+
+
+    public Users(){
+
+    }
+
+    public Users(String fullName, String email, String position, String phonenumber) {
+        this.fullName = fullName;
+        this.Email=email;
+        this.Position=position;
+        this.PhoneNumber=phonenumber;
+    }
+}
